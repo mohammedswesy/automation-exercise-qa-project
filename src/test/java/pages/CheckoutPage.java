3618732ed24055ff8aaef4b8153e53aef157eb77
Mohammed H. Al-Swesy <melswesy6@gmail.com>
2026-05-30 14:31:16 +0200

    Add TC22 checkout automation

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -1,5 +1,49 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CheckoutPage {
 
+    WebDriver driver;
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Recommended Item
+    By recommendedProductAddBtn =
+            By.xpath("(//a[contains(text(),'Add to cart')])[last()]");
+
+    By viewCartBtn =
+            By.xpath("//u[text()='View Cart']");
+
+    By cartProduct =
+            By.xpath("//td[@class='cart_description']");
+
+    // Actions
+
+    public void addRecommendedProductToCart() {
+        driver.findElement(recommendedProductAddBtn).click();
+    }
+
+    public void clickViewCart() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.elementToBeClickable(viewCartBtn));
+
+        driver.findElement(viewCartBtn).click();
+    }
+
+    // Validation
+
+    public boolean isProductDisplayedInCart() {
+        return driver.findElement(cartProduct).isDisplayed();
+    }
 }
--- src/test/java/tests/CheckoutTests.java
+++ src/test/java/tests/CheckoutTests.java
@@ -1,5 +1,26 @@
 package tests;
 
-public class CheckoutTests {
+import base.BaseTest;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CheckoutPage;
 
+public class CheckoutTests extends BaseTest {
+
+    @Test
+    public void addToCartFromRecommendedItems() {
+
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        checkoutPage.addRecommendedProductToCart();
+
+        checkoutPage.clickViewCart();
+
+        Assert.assertTrue(checkoutPage.isProductDisplayedInCart());
+    }
 }
