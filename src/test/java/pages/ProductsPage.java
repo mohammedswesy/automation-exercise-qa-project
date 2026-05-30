8201537d0f3797108c989405fe74ec9ef08965fc
BasilSaleem98 <basilsom1998@gmail.com>
2026-05-31 00:51:55 +0300

    Update T8, Upload TC9

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -11,26 +11,70 @@ import org.openqa.selenium.WebElement;
 public class ProductsPage {
 
 	WebDriver driver;
-	 
+	 											//All locators needed
+	
+												//locators for TC007
 	// home page locator
-    By homePage = By.xpath("//h2[normalize-space()='Features Items']");
+    By homePage = By.cssSelector("div[class='features_items'] h2[class='title text-center']");
+    
     // test cases button locator
     By testCase = By.linkText("Test Cases");
+    
     // to verify test case page is visable 
     By titleTestCases = By.xpath("//b[normalize-space()='Test Cases']");
+    
+    //variables 
+    boolean hPageExist, testCaseVis ;
+    
     /////////////////////////////////////////////////////////////////////////
+
+    											//locators for TC008
     //locator for product button
     By productBtn = By.xpath("//a[@href='/products']");
-    // to verify products page is visable 
-    By productsVisable = By.xpath("//h2[normalize-space()='All Products']");
+    
+    // to verify all products page is visable 
+    By productsVisable = By.xpath("//div[@class='features_items']/h2");
+    
     // to verify products list is visable 
-    By productsList = By.xpath("//h2[normalize-space()='All Products']");
+    By productsList = By.xpath("//div[@class='features_items']//div[@class='col-sm-4']");
+    
     // locator for first product
     By firstProduct = By.xpath("(//div[@class='features_items']//div[@class='choose']//a)[1]");
+    
     //locator for product details of first product is opened (image)
     By fProductDetailsPage = By.cssSelector("div[class='view-product'] img[alt='ecommerce website products']");
-  //locator for product details of first product is opened
-    By pDetailsVisable = By.cssSelector(".product-information");
+    
+    //locator for product details are visable
+    By productName = By.xpath("//div[@class='product-information']/h2");
+    By productCategory = By.xpath("//div[@class='product-details']//p[1]");
+    By productPrice = By.xpath("//div[@class='product-information']//span//span");
+    By productAvailability = By.xpath("//b[text()='Availability:']");
+    By productCondition = By.xpath("//b[text()='Condition:']");
+    By productBrand = By.xpath("//b[text()='Brand:']");
+    
+    //variables
+    boolean allProductsVis, fProductDetailsOpen, name, category, price, availability, condition, brand;
+	int productsListEmp;
+    
+    /////////////////////////////////////////////////////////////////////////
+										//locators for TC009
+    //locator for search bar
+    By searchBar = By.xpath("//input[@id='search_product']");
+    
+    //locator for search bar
+    By searchBtn = By.xpath("//button[@id='submit_search']");
+    
+    //locator for Searched Products header
+    By searchedProductsHeader = By.xpath("//h2[normalize-space()='Searched Products']");
+    
+    //locator for all items related to word searched in search bar
+    By searchedItems = By.xpath("//div[@class='single-products']//p");
+    
+    //variables
+    boolean searchedProHeadVis, allSearchedProVis;
+    //List<WebElement> productsRelated;
+    
+    //*********************************************************************************************************************\\
     
     //genral constructor
     public ProductsPage(WebDriver driver) {
@@ -41,8 +85,12 @@ public class ProductsPage {
 	///////////////////////////////////////////////TC007 Verify Test Cases Page	
     
     // verify home page is visible
-	public boolean homePageExist() {
-    	return driver.findElement(homePage).isDisplayed();
+	public boolean verifyHomePageExist() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("const elements = document.getElementsByClassName('adsbygoogle adsbygoogle-noablate'); while (elements.length > 0) elements[0].remove();");
+		js.executeScript("window.scrollBy(0, 300);");
+		hPageExist = driver.findElement(homePage).isDisplayed();
+    	return hPageExist;
     	
 	}
     
@@ -58,11 +106,17 @@ public class ProductsPage {
 
     // return if user is navigated to test cases page and displayed
 	public boolean verifyTestCases() {
-		return driver.findElement(titleTestCases).isDisplayed();
+		testCaseVis= driver.findElement(titleTestCases).isDisplayed();
+		return testCaseVis;
 		
 	}
 	
-	///////////////////////////////////////////////TC008 Verify Products Page	
+//	public void verifyTestCasesPage() {
+//		clickTestCase();
+//		
+//	}
+	
+	///////////////////////////////////////////////TC008 Verify All Products Page and product details	
 	
 	// click on product page
 	public void clickProductButton() {
@@ -75,39 +129,112 @@ public class ProductsPage {
 	
 	 // return if user is navigated to all produts page and displayed
 		public boolean verifyAllProducts() {
-			return driver.findElement(productsVisable).isDisplayed();
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("const elements = document.getElementsByClassName('adsbygoogle adsbygoogle-noablate'); while (elements.length > 0) elements[0].remove();");
+			allProductsVis= driver.findElement(productsVisable).isDisplayed();
+			return allProductsVis;
 			
 		}
 	
 		// return that the product list is empty
-		public boolean verifyProductsList() {
-			List<WebElement> productsList = driver.findElements(By.xpath("//div[@class='features_items']//div[@class='col-sm-4']"));
-			return productsList.isEmpty();
+		public int verifyProductsList() {
+			List<WebElement> productsLists = driver.findElements(productsList);
+			productsListEmp= productsLists.size();
+			return productsListEmp;
 			
 		}
 		
-		//view first product details 
+		//view first product details annd scroll for amount of pixels to pervent Ads
 		public void viewFirstProduct() {
-			driver.findElement(firstProduct).click();
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 			js.executeScript("const elements = document.getElementsByClassName('adsbygoogle adsbygoogle-noablate'); while (elements.length > 0) elements[0].remove();");
+			js.executeScript("window.scrollBy(0, 300);");
+			driver.findElement(firstProduct).click();
 			
 		}
 	
 		//view first product is opened
 		public boolean fProductDetailsPage() {
-			return driver.findElement(fProductDetailsPage).isDisplayed();
+			fProductDetailsOpen= driver.findElement(fProductDetailsPage).isDisplayed();
+			return fProductDetailsOpen;
 			
 		}
 	
 		//verify first product details are visible
-		public boolean fProductDetailsVis() {
-			return driver.findElement(pDetailsVisable).isDisplayed();
+		public boolean fProductName() {
+			name= driver.findElement(productName).isDisplayed();
+			return name;
 					
-			}
+		}
+		
+		public boolean fProductCata() {
+			category= driver.findElement(productCategory).isDisplayed();
+			return category;
+					
+		}
+		
+		public boolean fProductPrice() {
+			price= driver.findElement(productPrice).isDisplayed();
+			return price;
+					
+		}
+		
+		public boolean fProductAval() {
+			availability= driver.findElement(productAvailability).isDisplayed();
+			return availability;
+					
+		}
+		
+		public boolean fProductCondition() {
+			condition= driver.findElement(productCondition).isDisplayed();
+			return condition;
+					
+		}
+		
+		public boolean fProductBrand() {
+			brand= driver.findElement(productBrand).isDisplayed();
+			return brand;
+					
+		}
+		
+//		public void verifyAllProductsPageAndProductDetails() throws InterruptedException {
+//			Thread.sleep(500);
+////			verifyHomePageExist();
+//			Thread.sleep(500);
+//			clickProductButton();
+//			Thread.sleep(100); // wait for safe side
+////			verifyAllProducts();
+//			verifyProductsList();
+//			Thread.sleep(100); // wait for safe side
+//			viewFirstProduct();
+////			fProductDetailsPage();
+////			fProductDetailsVis();
+//			
+//		}
 	
+	///////////////////////////////////////////////////////TC9: Search Product
 	
-	
+		//enter product name in search bar "Top" and click
+		public void enterInputInSearch(String item) {
+			driver.findElement(searchBar).sendKeys(item);
+			driver.findElement(searchBtn).click();
+		}
+		
+		//verify Searched Products header is visible
+		public boolean searchProductsHeadVis() {
+			searchedProHeadVis= driver.findElement(searchedProductsHeader).isDisplayed();
+			return searchedProHeadVis;
+							
+		}
+		
+		// return that the product list is empty
+		public boolean sreachedItemRelated() {
+			List<WebElement> productsRelated = driver.findElements(searchedItems);
+			allSearchedProVis= productsRelated.isEmpty();
+			return allSearchedProVis;
+					
+		}
+		
 	
 	
 }
--- src/test/java/tests/ProductsTests.java
+++ src/test/java/tests/ProductsTests.java
@@ -14,51 +14,80 @@ public class ProductsTests extends BaseTest {
 		ProductsPage productsPage = new ProductsPage(driver);
 		
 		//see if the home page is displayed or not
-		Assert.assertEquals(productsPage.homePageExist(), true);
+		Assert.assertEquals(productsPage.verifyHomePageExist(), true, " Home page not displayed. ");
 		Thread.sleep(200);
 		
 		//click on test case button
-		productsPage.clickTestCase();
+		productsPage.verifyTestCases();
 		Thread.sleep(1000); // wait for safe side
 		
 		//see if the test case page is displayed or not
-		Assert.assertEquals(productsPage.verifyTestCases(), true);
+		Assert.assertEquals(productsPage.verifyTestCases(), true, " Test Cases page not displayed. ");
 		
 	}
 	
 											//TC008 Verify Products Page	
 	@Test
-	public void verifyAllProductsAndProductDetail () throws InterruptedException 
+	public void verifyAllProductsAndProductDetail () throws InterruptedException
 	{
 		ProductsPage productsPage = new ProductsPage(driver);
 		
 		//see if the home page is displayed or not
-		Assert.assertEquals(productsPage.homePageExist(), true);
+		Assert.assertEquals(productsPage.verifyHomePageExist(), true, " Home page not displayed. ");
 		
 		//click on products button
 		productsPage.clickProductButton();
 		Thread.sleep(100); // wait for safe side
 		
 		//see if the all products page is displayed or not
-		Assert.assertEquals(productsPage.verifyAllProducts(), true);
+		Assert.assertEquals(productsPage.verifyAllProducts(), true, "  All products not displayed. ");
 		
 		//verify that products list are visible
-		Assert.assertNotEquals(productsPage.verifyProductsList(), true);
+		Assert.assertNotEquals(productsPage.verifyProductsList(), true, " Products list not displayed. ");
 		Thread.sleep(100); // wait for safe side
 		
 		//view first product
 		productsPage.viewFirstProduct();
 		
 		//verify that product details is opend
-		Assert.assertEquals(productsPage.fProductDetailsPage(), true);
+		Assert.assertEquals(productsPage.fProductDetailsPage(), true, " Product details not opened. ");
 		
 		// verify that all product details are visible
-		Assert.assertEquals(productsPage.fProductDetailsVis(), true);
+		Assert.assertEquals(productsPage.fProductName(), true, " Name not displayed. ");
+		Assert.assertEquals(productsPage.fProductCata(), true, " Category not displayed. ");
+		Assert.assertEquals(productsPage.fProductPrice(), true, " Price not displayed. ");
+		Assert.assertEquals(productsPage.fProductAval(), true, " Availability not displayed. ");
+		Assert.assertEquals(productsPage.fProductCondition(), true, " Condition not displayed. ");
+		Assert.assertEquals(productsPage.fProductBrand(), true, " Brand not displayed. ");
 		
 	}
-	
-	
-	
+													//TC9: Search Product
+	@Test
+	public void searchProduct() throws InterruptedException 
+	{
+		ProductsPage productsPage = new ProductsPage(driver);
+		
+		//see if the home page is displayed or not
+		Assert.assertEquals(productsPage.verifyHomePageExist(), true, " Home page not displayed. ");
+		
+		//click on products button
+		productsPage.clickProductButton();
+		Thread.sleep(100); // wait for safe side
+		
+		//see if the all products page is displayed or not
+		Assert.assertEquals(productsPage.verifyAllProducts(), true, " ALL PRODUCTS page not displayed. ");
+		Thread.sleep(400); // wait for safe side
+		
+		//make input in search bar
+		productsPage.enterInputInSearch("Top");
+		
+		//see if the Searched Products is displayed or not
+		Assert.assertEquals(productsPage.searchProductsHeadVis(), true, " Searched Products heading not displayed. ");
+		
+		//see if the Searched Products whom related to search displayed
+		Assert.assertEquals(productsPage.sreachedItemRelated(), false, " Searched Products from search bar not displayed. ");
+		
+	}
 	
 	
 	
